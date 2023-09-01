e7edb12157b0e139735a0c285f026d26bcaed3ec
Promise Nwankwo <promisenwankwo125@gmail>
2023-09-01 04:39:57 +0100

    Defined Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,7 @@ export default function Navbar() {
   return (
     <header>
       <ul className="navigation-items">
-        <a>
-          {" "}
-          <li className="navigation-item">Lisbon</li>
-        </a>
+        <li className="navigation-item">Lisbon</li>
         <li className="navigation-item">Paris</li>
         <li className="navigation-item">Sydney</li>
         <li className="navigation-item">San Francisco</li>
